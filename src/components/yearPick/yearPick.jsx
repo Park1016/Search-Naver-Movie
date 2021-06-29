7d69d86b2637a71b7972b4f6b987dbc405c6bf89
hyunjeongPark <GuoHJP@gmail.com>
2021-06-30 03:05:47 +0900

    year reset 기능 구현

--- src/components/yearPick/yearPick.jsx
+++ src/components/yearPick/yearPick.jsx
@@ -1,73 +1,49 @@
-﻿import React, { memo, useRef } from 'react';
+﻿import React, { memo, useRef, useState } from 'react';
 import styles from './yearPick.module.css';
 import './yearPick.css';
 import { Rate, DatePicker, Space } from 'antd';
 import 'antd/dist/antd.css';
 import { getConfirmLocale } from 'antd/lib/modal/locale';
 
-// let year = new Date().getFullYear();
-// let firstYear = 0;
-// let lastYear = 1;
+let year = new Date().getFullYear();
 
 const YearPick = memo(({onYear, onReset}) => {
     const { RangePicker } = DatePicker;
     const pick = useRef();
+    let [prev, setPrev] = useState('');
 
-
-    const onInputClick = (e) => {
-        // let start = pick.current.firstElementChild.children.item(0).firstElementChild.value;
-        // let end = pick.current.firstElementChild.children.item(2).firstElementChild.value;
-        // let init = pick.current.firstElementChild.children.item(4);
-        // // console.log(e.target);
-        // if(start == '' && end == ''){
-        //     return;
-        // };
-        // if(e.target == init){
-        //     start = 0;
-        //     end = year;
-        // };
-        // onYear(start, end);
-
-        // console.log(e.target);
-        if(e.target.classList.contains('ant-picker')){
-            console.log(e.target.lastElementChild.firstElementChild.firstElementChild.viewBox);
-            if(e.target.lastElementChild.firstElementChild.firstElementChild.dataIcon == "close-circle"){
-            }
-            // onReset();
-        }
-    };
-    
-    const onInputChange = () => {
+    const onInputChange = () => {     
         let start = pick.current.firstElementChild.children.item(0).firstElementChild.value;
         let end = pick.current.firstElementChild.children.item(2).firstElementChild.value;
-        if(start == '' && end == ''){
-            return;
-        };
+    
+        if(prev === start){
+            onResetYear();
+        }
         if(start == '' && end != ''){
             start = end;
         }
         if(start != '' && end == ''){
             end = start;
-        }
+        }  
         onYear(start, end);
+        setPrev(start);
     };
 
-    // const onResetBtn = () => {
-    //     pick.current.firstElementChild.children.item(0).firstElementChild.value = '';
-    //     pick.current.firstElementChild.children.item(2).firstElementChild.value = '';
-    //     onReset();
-    // }
+    
+    const onResetYear = () => {
+        pick.current.firstElementChild.children.item(0).firstElementChild.value = '';
+        pick.current.firstElementChild.children.item(2).firstElementChild.value = '';
+        onReset();
+    }
 
     return (
-        // <section onClick={e=>onInputClick(e)}>
-        <section className={styles.yearPick}>
+        <section>
             <Space direction="vertical" size={12}>
                 <div ref={pick} className={styles.yearPick}>
                     <RangePicker onChange={onInputChange} picker="year" bordered={false} />
                 </div>
             </Space>
             {/* <Rate allowHalf defaultValue={0} /> */}
-            {/* <button className={styles.yearReset} onClick={onResetBtn}>reset</button> */}
         </section>
     )
 })
